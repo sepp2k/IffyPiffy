a35e47cdb2ac27f31301087d0aa3014e024e0582
Sebastian Hungerecker <sepp2k@googlemail.com>
2017-02-17 02:39:50 +0100

    Make compiler read from a file

--- compiler/src/ts/main.ts
+++ compiler/src/ts/main.ts
@@ -1,10 +1,25 @@
+import * as fs from 'fs';
 import {ANTLRInputStream} from 'antlr4ts/ANTLRInputStream';
 import {CommonTokenStream} from 'antlr4ts/CommonTokenStream';
 
 import {IffyPiffyParser} from './antlr-gen/IffyPiffyParser';
 import {IffyPiffyLexer} from './antlr-gen/IffyPiffyLexer';
 
-const lexer = new IffyPiffyLexer(new ANTLRInputStream("Room TestRoom {}"));
-const tokens = new CommonTokenStream(lexer);
-const parser = new IffyPiffyParser(tokens);
-console.log(parser.story());
+var appName = process.argv[0] + " " + process.argv[1];
+if(process.argv.length != 3) {
+    console.log("Usage: " + appName + " sourceFile");
+    process.exit(1);
+}
+
+const filename = process.argv[2];
+
+fs.readFile(filename, {encoding: "utf-8"}, function (err, source) {
+    if(err) {
+        console.log("Couldn't open the source file: " + err);
+        process.exit(2);
+    }
+    const lexer = new IffyPiffyLexer(new ANTLRInputStream(source));
+    const tokens = new CommonTokenStream(lexer);
+    const parser = new IffyPiffyParser(tokens);
+    console.log(parser.story());
+});
