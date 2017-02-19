1d72698281352bf58c3b15cd8c8b2cec144b5dbb
Sebastian Hungerecker <sepp2k@googlemail.com>
2017-02-19 16:31:28 +0100

    Add the beginnings of an AST type

---
+++ compiler/src/ts/ast.ts
@@ -0,0 +1,56 @@
+export interface Story {
+    statements: TopLevelStatement[]
+}
+
+export type TopLevelStatement = Definition | Assignment;
+export type Statement = TopLevelStatement | Expression;
+
+export interface Definition {
+    kind: "Definition";
+    name: LExpression;
+    body: Statement[];
+}
+
+export interface Assignment {
+    kind: "Assignment";
+    lhs: LExpression;
+    rhs: Expression;
+}
+
+export type LExpression = Variable | LMemberAccess | LArrayAccess;
+export type Expression = Variable | MemberAccess | ArrayAccess | FunctionCall;
+
+export interface Variable {
+    kind: "Variable";
+    name: String;
+}
+
+export interface MemberAccess {
+    kind: "MemberAccess";
+    receiver: Expression;
+    memberName: String;
+}
+
+export interface ArrayAccess {
+    kind: "ArrayAccess";
+    receiver: Expression;
+    indexExpression: Expression;
+}
+
+export interface LMemberAccess extends MemberAccess {
+    kind: "MemberAccess";
+    receiver: LExpression;
+    memberName: String;
+}
+
+export interface LArrayAccess extends ArrayAccess {
+    kind: "ArrayAccess";
+    receiver: LExpression;
+    indexExpression: Expression;
+}
+
+export interface FunctionCall {
+    kind: "FunctionCall";
+    func: Expression;
+    arguments: Expression[];
+}
--- compiler/src/ts/parser.d.ts
+++ compiler/src/ts/parser.d.ts
@@ -1,1 +1,3 @@
-export declare var parser: { parse: (x: string) => {} }
+import * as ast from './ast'
+
+export declare var parser: { parse: (x: string) => ast.Story }
