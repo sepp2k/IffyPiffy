a334f2c2d17fa5466f598e29fff00ade40a58ea7
Sebastian Hungerecker <sepp2k@googlemail.com>
2017-03-03 00:32:37 +0100

    Make compiler accept more command line arguments
    Instead of specifying one story file, you can now specify arbitrarily
    many. You can also use the --print-ast flag to specify whether the AST
    of the story file(s) should be printed.

--- compiler/src/ts/main.ts
+++ compiler/src/ts/main.ts
@@ -1,25 +1,31 @@
 import "source-map-support/register";
 import * as fs from "fs";
-import {p} from "./util";
-import {parser} from "./parser";
+import { p } from "./util";
+import { parser } from "./parser";
 import * as codegen from "./codegen";
 
 let appName = process.argv[0] + " " + process.argv[1];
-if(process.argv.length !== 3) {
-    console.log("Usage: " + appName + " sourceFile");
+if (process.argv.length < 3) {
+    console.log("Usage: " + appName + " [--print-ast] sourceFiles");
     process.exit(1);
 }
 
-const filename = process.argv[2];
-const outfile = filename.replace(/\.iffypiffy$/, "") + ".js";
-
-fs.readFile(filename, {encoding: "utf-8"}, function (err, source) {
-    if(err) {
-        console.log("Couldn't open the source file: " + err);
-        process.exit(2);
+let printAst = false;
+for (let filename of process.argv.slice(2)) {
+    if(filename === "--print-ast") {
+        printAst = true;
+        continue;
     }
-    let story = parser.parse(source);
-    p(story);
-    let js = codegen.generateJS(story);
-    fs.writeFileSync(outfile, js);
-});
+    const outfile = filename.replace(/\.iffypiffy$/, "") + ".js";
+
+    fs.readFile(filename, { encoding: "utf-8" }, function (err, source) {
+        if (err) {
+            console.log("Couldn't open the source file: " + err);
+            process.exit(2);
+        }
+        let story = parser.parse(source);
+        if(printAst) p(story);
+        let js = codegen.generateJS(story);
+        fs.writeFileSync(outfile, js);
+    });
+}
